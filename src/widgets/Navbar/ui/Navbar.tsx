fc2ccf0147f90d4d5050419d2ebb9ce752a8f6a0
Daniel Toint <Gedztodu@gmail.com>
2023-02-14 16:29:33 +0200

    Refactor header

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,21 +1,26 @@
+import { useTranslation } from 'react-i18next';
 import classNames from 'shared/lib/classNames';
 import AppLink, { AppLinkTheme } from 'shared/ui/AppLink/AppLink';
-import { ThemeToggler } from 'widgets/ThemeToggler';
+import React from 'react';
 import styles from './Navbar.module.scss';
 
 interface NavbarProps {
   className?: string;
 }
 
-const Navbar: React.FC<NavbarProps> = ({ className }) => (
-  <header className={classNames(styles.navbar, {}, [className])}>
-    <AppLink to="/" theme={AppLinkTheme.PRIMARY}>
-      Home
-    </AppLink>
-    <AppLink to="/about" theme={AppLinkTheme.SECONDARY}>
-      About
-    </AppLink>
-  </header>
-);
+const Navbar: React.FC<NavbarProps> = ({ className }) => {
+  const { t } = useTranslation();
+
+  return (
+    <header className={classNames(styles.navbar, {}, [className])}>
+      <AppLink to="/" theme={AppLinkTheme.PRIMARY}>
+        {t('home')}
+      </AppLink>
+      <AppLink to="/about" theme={AppLinkTheme.SECONDARY}>
+        {t('about')}
+      </AppLink>
+    </header>
+  );
+};
 
 export default Navbar;
